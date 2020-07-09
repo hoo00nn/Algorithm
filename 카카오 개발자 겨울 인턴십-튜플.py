f7ea705d8caa6874e346d807c995dde2944d8a18
hoo00nn <hoo00nn@naver.com>
2020-07-09 18:07:49 +0900

    Algorithm

--- 카카오 개발자 겨울 인턴십-튜플.py
+++ 카카오 개발자 겨울 인턴십-튜플.py
@@ -1,3 +1,26 @@
+# lstrip, rstrip, split 함수 사용 했으면 훨씬 간결했다
+# 문자열 함수 자주 사용해보기
+# 간결하게 수정한 코드
+
+
+def solution(s):
+    answer = []
+    ret = []
+    s = sorted(s.lstrip('{').rstrip('}').split('},{'), key=lambda x: len(x))
+
+    for i in s:
+        answer.append(i.split(','))
+
+    for i in answer:
+        for j in i:
+            if int(j) not in ret:
+                ret.append(int(j))
+
+    return ret
+
+
+# 밑에서부턴 처음 내가 짠 코
+
 def solution(s):
     answer = []
     stack = []
