74d8b7a6361af6d921bb9cbb260e23358ebb4e9f
hoo00nn <hoo00nn@naver.com>
2020-07-14 23:57:32 +0900

    Algorithm

--- Greedy-구명보트(Level2).py
+++ Greedy-구명보트(Level2).py
@@ -1,3 +1,7 @@
+# pop() 사용해서 문제를 풀어보니 효율성에서 한 문제가 실패했다.
+# pop() 사용하지 않고 인덱스를 직접 조종해서 풀었다.
+
+
 def solution(people, limit):
     answer = 0
     people.sort()
@@ -16,6 +20,25 @@ def solution(people, limit):
                 answer += 1
                 return answer
 
+        answer += 1
+
+    return answer
+
+# 최종 코
+
+def solution(people, limit):
+    answer = 0
+    start = 0
+    end = len(people) - 1
+    people.sort()
+
+    while start <= end:
+        if people[start] + people[end] <= limit:
+            answer += 1
+            start += 1
+            end -= 1
+            continue
+        end -= 1
         answer += 1
 
     return answer
